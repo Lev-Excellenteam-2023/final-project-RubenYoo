8f2dab589a17f1cbbbce69f02a67667ce1be6c24
RubenYoo <rubenchiche0317@gmail.com>
2023-05-18 20:46:34 +0300

    Constructor of the GptExplainer class

--- gpt-explainer.py
+++ gpt-explainer.py
@@ -1,10 +1,19 @@
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-token = os.getenv('OPENAI_TOKEN')
+import openai
 
 
 class GptExplainer:
     def __init__(self):
-        pass
+        load_dotenv()
+        openai.api_key = os.getenv('OPENAI_TOKEN')
+        self.model = "gpt-3.5-turbo"
+        self.gpt_context = [
+            {"role": "system",
+             "content":
+             "You are a lecturer, that take in input the text of a powerpoint presentation slide, and you need "
+             "to explain it"},
+        ]
+
+
+
