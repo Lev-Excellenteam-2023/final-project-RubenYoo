7349fc367bf7069303f050e765125d72aee08e71
RubenYoo <rubenchiche0317@gmail.com>
2023-05-18 16:10:39 +0300

    Created the extract text from slide method

--- powerpoint_parser.py
+++ powerpoint_parser.py
@@ -1,10 +1,29 @@
-import pptx
+from pptx import Presentation
+from collections.abc import Generator
 import os
 
 
 class PowerpointParser:
-    def __int__(self, file_path: str) -> None:
+    pptx_file: str
+
+    def __init__(self, file_path: str) -> None:
         if os.path.isfile(file_path):
             self.pptx_file = file_path
         else:
             raise FileNotFoundError("The powerpoint file was not found")
+
+    def extract_text_from_slide(self) -> Generator:
+        with open(self.pptx_file, "rb") as presentation_file:
+            presentation = Presentation(presentation_file)
+
+            for slide in presentation.slides:
+                slide_text = ""
+                for shape in slide.shapes:
+                    if shape.has_text_frame:
+                        for paragraph in shape.text_frame.paragraphs:
+                            for run in paragraph.runs:
+                                slide_text += run.text
+
+                yield slide_text
+
+
