9aefcdbc488f819c4c3e7f78c57f44b963b5a073
RubenYoo <rubenchiche0317@gmail.com>
2023-05-19 14:10:14 +0300

    docs of the class

--- powerpoint_parser.py
+++ powerpoint_parser.py
@@ -4,18 +4,33 @@ import os
 
 
 class PowerpointParser:
+    """
+    This class is used to parse a powerpoint file and extract the text from each slide
+    """
     pptx_file: str
 
     def __init__(self, file_path: str) -> None:
+        """
+        Initialize the class
+        :param file_path: The path to the PowerPoint file
+        """
+
         if os.path.isfile(file_path):
             self.pptx_file = file_path
         else:
             raise FileNotFoundError("The powerpoint file was not found")
 
     def extract_text_from_slide(self) -> Generator:
+        """
+        Extract the text from each slide
+        :return: A generator that yields the text from each slide
+        """
+
+        # Open the PowerPoint file
         with open(self.pptx_file, "rb") as presentation_file:
             presentation = Presentation(presentation_file)
 
+            # Iterate through each slide and extract the text
             for slide in presentation.slides:
                 slide_text = ""
                 for shape in slide.shapes:
