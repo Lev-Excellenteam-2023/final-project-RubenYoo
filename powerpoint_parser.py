e88fae098e6745d77d8b277f28ab0fd3ef80c9fe
RubenYoo <rubenchiche0317@gmail.com>
2023-05-21 20:18:15 +0300

    docs

--- powerpoint_parser.py
+++ powerpoint_parser.py
@@ -44,10 +44,13 @@ class PowerpointParser:
 
 
 def main():
+    # Get the path to the PowerPoint file
     path = input("Enter your powerpoint file path\n")
 
+    # Create a PowerpointParser object
     pptx_object = PowerpointParser(path)
 
+    # Iterate through each slide and print the results
     for slide, text in enumerate(pptx_object.extract_text_from_slide()):
         print(f"[slide {slide}]{text}")
 
