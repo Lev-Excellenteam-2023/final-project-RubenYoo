e9cd7627fc6c0784af46cf793da72420726194a4
RubenYoo <rubenchiche0317@gmail.com>
2023-05-21 21:53:51 +0300

    fixing the save to json func

--- gather_explanations_to_json.py
+++ gather_explanations_to_json.py
@@ -3,9 +3,10 @@ import os
 
 
 def sort_explanations(list_of_explanations: list) -> list:
-    return sorted(list_of_explanations, key=lambda x: x[0])
+    sorted_list = sorted(list_of_explanations, key=lambda x: x[0])
+    return [slide_explanation[1] for slide_explanation in sorted_list]
 
 
 def save_to_json(list_of_explanations: list, path: str) -> None:
     with open(os.path.splitext(os.path.basename(path))[0] + '.json', 'w') as f:
-        json.dump(sort_explanations(list_of_explanations[1]), f)
+        json.dump(sort_explanations(list_of_explanations), f)
