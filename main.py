d6892659751c3611a0e23a85a890a4a8cdad5179
RubenYoo <rubenchiche0317@gmail.com>
2023-05-21 20:42:08 +0300

    starting the implementation of the main

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import powerpoint_parser
+import gpt_explainer
+import asyncio
 
 
-def main():
-    print("Hello World!")
+async def main():
+    # Get the path to the PowerPoint file
+    path = input("Enter your powerpoint file path\n")
+
+    # Create a PowerpointParser object
+    pptx_object = powerpoint_parser.PowerpointParser(path)
+
+    # Create a GPTExplainer object
+    gpt_object = gpt_explainer.GptExplainer()
+
+    # Iterate through each slide and send the slide text to the GPTExplainer object
+    for slide, text in enumerate(pptx_object.extract_text_from_slide()):
+        await gpt_object.send_slide_text_to_gpt(slide, text)
+
+    # Print the results
+    print(gpt_object.get_explanations_slides())
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
