13ad4c281bcb9660928b2cf6a2f333d0c4a18216
RubenYoo <rubenchiche0317@gmail.com>
2023-06-11 20:05:57 +0300

    starting the implementation of the upload

--- Web_api/app.py
+++ Web_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def get_home_page():
     return 'Hello world'
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+
+    file = request.files['file']  # Access the uploaded file
+    print(file)
+
+    return 'File uploaded successfully!'
 
 
 if __name__ == '__main__':
