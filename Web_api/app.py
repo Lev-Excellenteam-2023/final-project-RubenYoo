6dacc10f55c1615c86cb19d662d3e9a8d58e8dea
RubenYoo <rubenchiche0317@gmail.com>
2023-06-11 20:21:41 +0300

    upload file with creating a uid and a timestamp

--- Web_api/app.py
+++ Web_api/app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request
+import uuid
+import time
 
 app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def get_home_page():
-    return 'Hello world'
 
 
 @app.route('/', methods=['POST'])
 def upload_file():
 
-    file = request.files['file']  # Access the uploaded file
-    print(file)
+    # Access the uploaded file
+    file = request.files['file']
+    timestamp = time.time()
 
-    return 'File uploaded successfully!'
+    secret_string = file.filename + str(timestamp)
+    namespace = uuid.uuid4()
+
+    # Generate a random UUID as the namespace
+    uid = uuid.uuid5(namespace, secret_string)
+
+    # Save the file
+    file.save(f'uploads/[{uid}][{timestamp}][{file.filename}].pptx')
+
+    return {'uid': str(uid)}
 
 
 if __name__ == '__main__':
