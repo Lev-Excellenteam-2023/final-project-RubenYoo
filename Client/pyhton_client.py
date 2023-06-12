a6330cbe7b18216f8cd86093992b791b47c77740
RubenYoo <rubenchiche0317@gmail.com>
2023-06-12 13:04:03 +0300

    added exception handling

--- Client/pyhton_client.py
+++ Client/pyhton_client.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 
 URL = 'http://127.0.0.1:5000/'
 
@@ -11,15 +10,21 @@ def send_file(path):
 
     if response.status_code == 200:
         return response.json()['uid']
+    else:
+        raise Exception(response.text)
 
 
 def main():
     file_path = input("Enter the PowerPoint file path\n")
-    if os.path.exists(file_path):
-        send_file(file_path)
-    else:
+
+    if not os.path.exists(file_path):
         print("this file not exist")
         exit()
+
+    try:
+        rep = send_file(file_path)
+    except Exception as e:
+        print(f"Error {e}")
 
 
 if __name__ == '__main__':
